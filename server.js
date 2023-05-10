9f83a7df48974fac978107fea6814758b6affd6e
taifong4 <86180004+taifong4@users.noreply.github.com>
2023-05-10 15:10:18 +0700

    Update server.js

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ cors_proxy.createServer({
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
-//     'cookie',
-//     'cookie2',
+    'cookie',
+    'cookie2',
     // Strip Heroku-specific headers
     'x-request-start',
     'x-request-id',
